3bb42da9b7b74a8a5197f9966da693bd9082b9e0
Jake Swanson <jakswa@gmail.com>
2014-04-24 01:28:48 -0400

    adding some tests

---
+++ test/mailpress.js
@@ -0,0 +1,27 @@
+var assert = require('assert');
+var sinon = require('sinon');
+var path = require('path');
+var Mailpress = require('./../index');
+
+describe('Mailpress', function() {
+    describe('with no base configs', function() {
+      var badmailer;
+      before(function(done) {
+        badmailer = new Mailpress();
+        done();
+      });
+      it('should not work', function(done) {
+        var spy = sinon.spy();
+        badmailer.sendMail().catch(spy).lastly(function() {
+          assert(spy.called);
+          done();
+        });
+      });
+      it('should have basename of current file', function(done) {
+        var basename = path.basename(__filename).split('.')[0];
+        var config = badmailer.defaults.templateBaseName;
+        assert(config === basename, config);
+        done();
+      });
+    });
+});
