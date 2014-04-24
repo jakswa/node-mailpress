0d7ce58eb5334ae5821d699336eb27ea29d38338
Jake Swanson <jakswa@gmail.com>
2014-04-24 01:28:29 -0400

    switching to bluebird promises (won me over)

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var nodemailer = require("nodemailer");
 var config = require("config");
 var mustache = require("mustache");
 var _ = require('underscore');
-var q = require('q');
+var Promise = require('bluebird');
 var fs = require('fs');
 var path = require('path');
 
@@ -15,7 +15,7 @@ var nodeMailerOpts = [
 module.exports = function (opts) {
   "use strict";
 
-  var conf = config.mailer;
+  var conf = config.mailer || {};
   var transport = nodemailer.createTransport(conf.transport, conf.transportOpts);
 
   // assuming by default you'll name your templates the same as the mailer,
@@ -32,17 +32,17 @@ module.exports = function (opts) {
   _.extend(defaults, conf, opts); // user can override all the above
 
   // populate templates if not provided inline
-  var templatesLoaded = q.all(_.map(defaults.templateExtensions, function(fileExt, version) {
+  var templatesLoaded = Promise.all(_.map(defaults.templateExtensions, function(fileExt, version) {
     if (defaults.templates[version] || defaults[version]) {
       return q.when(); // assume user provided their own template to render
     }
-    var templatePromise = q.defer();
+    var templatePromise = Promise.defer();
     var fileName = defaults.templateBaseName + fileExt;
     var filePath = path.resolve(rootDir, defaults.templateDir, fileName);
     fs.readFile(filePath, function(err, data) {
       if (err) {
         // they need to provide a text/html template! or else! (...or do they?)
-        templatePromise.reject(new Error("Error loading " + version + " template: " + err));
+        templatePromise.reject("Error loading " + version + " template: " + err);
       } else {
         defaults.templates[version] = data.toString();
         templatePromise.resolve();
@@ -54,7 +54,7 @@ module.exports = function (opts) {
   this.sendMail = function(locals, mailOpts) {
     return templatesLoaded.then(function() {
       var conf = _.extend({}, defaults, mailOpts);
-      var defer = q.defer();
+      var defer = Promise.cast();
 
       // for each of the templates defined above (html, text, or both),
       // render the template using the locals provided
